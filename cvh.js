085f786d6d695f8b953511f4b45f566e4d431b5b
MaxMahem <MaxMahem@Win-Mahem>
2012-08-06 01:46:42 -0500

    Version 1.3! Voting is implemented! Decided to not use javascript for now, only allowing single answer questions as currently.

---
+++ cvh.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+    
+// This function is not currently necessary, but preserved in case we move to
+// javascript in order to allow two options to be picked.
+//    /* when the answer div is clicked */
+//    $('.answer').click(function(event) {
+//        /* remove selected class from all answers */
+//        $('.answer').removeClass("selected");
+//
+//        /* add 'selected' class to clicked answer */
+//        $(this).addClass("selected");
+//        
+//        /* toggle the state of the option */
+//        var optionBox = $(this).find("input");
+//        optionBox.prop("checked", !optionBox.prop("checked"));
+//    });
+});
